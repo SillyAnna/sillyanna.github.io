2d3cb3313cdd9138c4801457c9f8515367d10e9c
Anna <61844821+SillyAnna@users.noreply.github.com>
2026-01-29 16:29:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -243,7 +243,8 @@ document.addEventListener('DOMContentLoaded', () => {
     "\"Love and online numbers are her coping mechanisms.\"",
     "\"I'm a defect, surgical project.\"",
     "\"I am an angel sent down from above. Praise be the heavens, Praise me I'm the lord.\"",
-    "\"Everything means nothing to me.\""
+    "\"Everything means nothing to me.\"",
+    "\"I can't do well when I think you're going to leave me but I know I try. Are you going to leave me now?\""
   ];
   let bioText = '';
   let bioIndex = 0;
@@ -422,6 +423,7 @@ document.addEventListener('DOMContentLoaded', () => {
   typeWriterStart();
 
 });
+
 
 
 
